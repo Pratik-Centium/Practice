c06771b20dcf3b3baffbb69e8530a63c33d85f2e
Pratik-Centium <pratik.padale@centium.net>
2022-07-29 15:59:22 +0530

    first commit

---
+++ StringHeap.java
@@ -0,0 +1,23 @@
+
+public class StringHeap {
+
+	 public static void concat1(String s1)
+	    {
+	        s1 = s1 + "forgeeks";
+	        System.out.println("in funcation : "+s1);
+	        
+	    }
+	 
+	public static void main(String[] args) {
+
+		String s1="pratik";
+		
+		concat1(s1);
+		
+		
+		
+		System.out.println(s1);
+
+	}
+
+}

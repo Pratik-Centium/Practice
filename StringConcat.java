49d535a6a035bd0d3cb97024ab365f9207c69f7c
Pratik-Centium <pratik.padale@centium.net>
2022-07-29 15:45:20 +0530

    first commit

---
+++ StringConcat.java
@@ -0,0 +1,16 @@
+
+public class StringConcat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="pratik";
+		String s2="padale";
+		
+		String s3=s1.concat(s2);
+		String s4=s1+s2;
+		System.out.println(s3);
+		System.out.println(s4);
+
+	}
+
+}
